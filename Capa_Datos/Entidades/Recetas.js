bdda5028839c9e4a9404801595cf363655f4ee14
luissalda1995 <luis.salda.1995@gmail.com>
2015-04-13 18:51:50 -0500

    Entidades

--- Capa_Datos/Entidades/Recetas.js
+++ Capa_Datos/Entidades/Recetas.js
@@ -1,1 +1,10 @@
-
+function Recetas(id,duracion,nombre,dificultad,cantidad_personas,paso_paso,ingredientes){
+	var id,duracion,nombre,dificultad,cantidad_personas,paso_paso,ingredientes;
+	this.id = id;
+	this.duracion = duracion;
+	this.nombre = nombre;
+	this.dificultad = dificultad;
+	this.cantidad_personas = cantidad_personas;
+	this.paso_paso = paso_paso;
+	this.ingredientes = ingredientes;
+}
--- Capa_Datos/Entidades/Usuarios.js
+++ Capa_Datos/Entidades/Usuarios.js
@@ -1,1 +1,9 @@
-
+function Usuario(id,nombre,correo,telefono,contraseña,sexo){
+	var id,nombre,correo,telefono,contraseña,sexo;
+	this.id = id;
+	this.nombre = nombre;
+	this.correo = correo;
+	this.telefono = telefono;
+	this.contraseña = contraseña;
+	this.sexo = sexo;
+}
