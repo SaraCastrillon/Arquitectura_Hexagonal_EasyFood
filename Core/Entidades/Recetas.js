0f828727dffb1dba3f6da733f44cb7078f6a631b
Ana María Ramirez Arboleda <anita-0621@hotmail.com>
2015-04-15 16:59:29 -0500

    corecciones y agregaciones

--- Core/Entidades/Recetas.js
+++ Core/Entidades/Recetas.js
@@ -1,6 +1,8 @@
-function Receta(nombre,descripcion,image){
-	var nombre,descripcion,image;
+function Receta(nombre,descripcion,duracion, calificacion, image){
+	var nombre,descripcion,image, duracion, calificacion;
 	this.nombre = nombre;
 	this.descripcion = descripcion;
 	this.image = image;
+	this.duracion = duracion;
+	this.calificacion =califiacion;
 }
