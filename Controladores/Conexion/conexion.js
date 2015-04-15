b4621d19356879af243640f745c69dc4feae03c3
luissalda1995 <luis.salda.1995@gmail.com>
2015-04-15 16:56:26 -0500

    conexiones

--- Controladores/Conexion/conexion.js
+++ Controladores/Conexion/conexion.js
@@ -1,14 +1,34 @@
-var datos;
-function conectar(){
+function conectarRecetas(){
 	$.ajax({
 		method: "GET",
 		url: "http://54.186.255.219/easyfood/querys/listarbd.php?tipo=listarrecetas",
 		dataType: 'xml',
 		success:function(data){
 			datos = data;
+			return data;
 		},
 		error:function(){
 			alert("error");
+		}
+	});
+}
+
+function conectarUsuarios(user,pass){
+	$.ajax({
+		method: 'POST',
+		data: {usuario:user,clave:pass},	
+		url:"http://54.186.255.219/easyfood/querys/listarbd.php?tipo=validarusuario",
+		success: function (data) {
+			if(data == "si"){
+				return true;
+			}else{
+				return false;
+			}
+			
+		},
+		error: function (error){
+			$.mobile.hidePageLoadingMsg();
+			alert("Ocurrio un problema");
 		}
 	});
 }
